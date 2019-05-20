b774eb3ef88e5733148c72dfb0e83d2b7fffc902
Dean Lovett <dean.lovett@gmail.com>
2019-05-20 13:40:10 -0700

    adds comments & removes debug code & pretties up the code

---
+++ README.md
@@ -0,0 +1,6 @@
+# node101-movie-finder-data
+
+An exercise for San Diego Code School's Fullstack Javascript Web Developer Bootcamp.
+
+A simple server that will look up movies in the OMDB via ID or name.  
+The simple server will also internally cache the data from queries using a map.
--- server/app.js
+++ server/app.js
@@ -10,80 +10,51 @@ const app = express();
 
 // When making calls to the OMDB API make sure to append the '&apikey=8730e0e' parameter
 
-const omdbap = 'http://www.omdbapi.com/' ;
+const omdbapi = 'http://www.omdbapi.com/' ;
 const apiKey = '&apikey=8730e0e';
 
 var movieIDs = new Map();
 var movieTitles = new Map();
 
-app.use(logger('dev')); // logger
+app.use(logger('dev'));
 
 app.get('/', (req, res) => {
-    var movieID = req.query.i; // hey req, do you have something after "i"
-    var movieTitle = encodeURIComponent( req.query.t ); // hey req, do you have something after "j"
-    if( movieID ){
-        if(_debug_) {
-            console.log('\t\t#### movieID  ##############################################');
-            console.log(`\t\t  omdbap: ${omdbap}`);
-            console.log('\t\t     ?i=');
-            console.log(`\t\t movieID: ${movieID}`);
-            console.log(`\t\t  apiKey: ${apiKey}` );
-            console.log('\t\t===========================================');
-            console.log('\t\t---- is this movie ID our map? (cache) ----');
-        }
-        if( movieIDs.has( movieID ) ){
-            if(_debug_) {
-                console.log('\t\t\t---- YES, give user...         the movie ----');
-            }
-            res.send( movieIDs.get( movieID ) );
-        }else{
-            if(_debug_) {
-                console.log('\t\t\t----  NO, use axios to ...');
-                console.log('\t\t\t-----                 get the movie ----');
-                console.log('\t\t\t----            send user the movie ----');
-                console.log('\t\t\t----                cache the movie ----');
-            }
+    const movieID = req.query.i; // hey req, do you have something after "i"?
+    const movieTitle = encodeURIComponent( req.query.t ); // hey req, do you have something after "j"? By-the-way, encode it.
 
-            axios.get( omdbap + '?i=' + movieID + apiKey )
-                .then(ax_res => {
+    if( movieID ){
+        if( movieIDs.has( movieID ) ){
+            res.send( 
+                movieIDs.get( movieID ) 
+            );
+        }
+        else{
+            axios
+                .get( omdbapi + '?i=' + movieID + apiKey )
+                .then( ax_res => {
                     res.send( ax_res.data ) ;
                     movieIDs.set( movieID, ax_res.data ); // cache it!
                 })
-                .catch(error =>{
-                    console.log('##################################################');
+                .catch( error => {
                     console.log('error: an error occurred handling Movie ID search.');
                     console.log('error',error);
                 });
         }
-    }else if( movieTitle ){
-        if(_debug_) {
-            console.log('\t\t#### movieTitle  ##############################################');
-            console.log(`\t\t     omdbap: ${omdbap}`);
-            console.log('\t\t        ?t=');
-            console.log(`\t\t movieTitle: ${movieTitle}`);
-            console.log(`\t\t     apiKey: ${apiKey}` );
-            console.log('\t\t===========================================');
-            console.log('\t\t---- is this movie ID our map? (cache) ----');
+    }
+    else if( movieTitle ){
+        if( movieTitles.has( movieTitle  )){
+            res.send( 
+                movieTitles.get( movieTitle ) 
+            );
         }
-        if( movieTitles.has( movieTitle )){
-            if(_debug_) {
-                console.log('\t\t\t---- YES, give user...         the movie ----');
-            }
-            res.send( movieTitles.get( movieTitle ) );
-        }else{
-            if(_debug_) {
-                console.log('\t\t\t----  NO, use axios to ...');
-                console.log('\t\t\t-----                 get the movie ----');
-                console.log('\t\t\t----            send user the movie ----');
-                console.log('\t\t\t----                cache the movie ----');
-            }
-            axios.get( omdbap + '?t=' + movieTitle + apiKey)
+        else{
+            axios
+                .get( omdbapi + '?t=' + movieTitle + apiKey )
                 .then( ax_res => {
                     res.send( ax_res.data );
-                    movieTitles.set( movieTitle, ax_res.data); // cache it
+                    movieTitles.set( movieTitle, ax_res.data ); // cache it
                 })
-                .catch( error =>{
-                    console.log('#####################################################');
+                .catch( error => {
                     console.log('error: an error occurred handling Movie Title search.')
                     console.log('error', error );
                 });
